cd7d5f0656d57e68c99ed7f33c0e96d68ede28ed
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2017-06-16 13:21:51 -0400

    merge check

--- 04_gen_codebook.R
+++ 04_gen_codebook.R
@@ -14,10 +14,24 @@ sq <- read_excel("data/output/meta/Labels_2016.xlsx") %>%
   select(Name, Label) %>%
   mutate(sOrder = 1:n())
 
+# nathan and liz
+nl <- read_csv("data/source/2016_guidebook_variables_orderedby2014.csv")
 
 
-# Join 
+# Join  crunch and stata
 cs <- left_join(cq, sq, by = c("alias" = "Name"))
+
+
+
+# compare with 2014  ------
+nl_key <- select(nl, rowID, code14, code16)
+
+
+# how much of code16 acutally matches to crunch?
+anti_join(nl_key, sq, by = c("code16" = "Name"))
+
+# anti_join(cq, nl_key, by = c("alias"  = "code16"))
+
 
 
 saveRDS(cs, "data/output/meta/fmt_metadata_cc16_stata.Rds")
