f68f559e379b5b3a9fd88232942b2a24c313fdec
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2017-07-07 16:57:15 +0900

    distinguish post recall and pre-election intent

--- build/01_stack.R
+++ build/01_stack.R
@@ -6,7 +6,26 @@ library(readr)
 library(foreach)
 
 
+# READ ------
 
+# 2012 and before (compiled by Stephen Pettigrew and others)
+ccp <- std_dv("data/source/cces/2006_2012_cumulative.dta", 
+              guess_year = FALSE)
+
+
+
+# take the needed columns for 2013- 2016
+cc13 <- std_dv("data/source/cces/2013_cc.dta")
+cc14 <- std_dv("data/source/cces/2014_cc.dta")
+cc15 <- std_dv("data/source/cces/2015_cc.dta")
+cc16 <- std_dv("data/source/cces/2016_cc.dta")
+
+
+# helper data -- 
+statecode <- read_csv("~/Dropbox/cces_rollcall/data/source/statecode.csv")
+
+
+# functions ----- 
 # rudimentary standardization for data that comes out of dataverse
 std_dv <- function(path, guess_year = TRUE) {
   
@@ -106,30 +125,56 @@ stdName <- function(tbl){
                zipcode = zip_pre,
                countyFIPS = county_fips_pre,
                reg_true = reg_validation,
-               reg_self = registered_pre)
+               reg_self = registered_pre,
+               validt_trn = gen_validated,
+               voted_pres_08 = vote_pres_08,
+               voted_rep = vote_house, 
+               voted_sen = vote_sen,
+               voted_gov = vote_gov,
+               # intent_trn = vote_intent_general,
+               intent_pres_08 = vote_intent_pres_08,
+               intent_pres_12 = vote_intent_pres_12,
+               intent_rep = vote_intent_house,
+               intent_sen = vote_intent_senate,
+               intent_gov = vote_intent_gov)
     }
     
     
     if (identical(cces_year, 2013L)) {
       tbl <- tbl %>% mutate(fips = floor(as.numeric(countyfips)/1000),
                             cdid = as.numeric(cdid113)) %>% 
-        rename(approval_rep = CC13_313a,
+        rename(approval_pres = CC312a,
+               approval_rep = CC13_313a,
                approval_sen1 = CC13_313b,
                approval_sen2 = CC13_313c,
-               vote_pres_12 = CC13_315) %>% 
+               approval_gov = CC312d,
+               voted_pres_12 = CC13_315) %>% 
         left_join(statecode, by = "fips")
     }
     
     if (identical(cces_year, 2014L)) {
       tbl <- rename(tbl, 
                     approval_rep = CC14_315a,
-                    vote_pres_12 = CC14_317)
+                    approval_sen1 = CC14_315b,
+                    # approval_sen2 = CC14_315c,
+                    approval_gov = CC14_308d,
+                    vote_rep = CC412,
+                    voted_pres_12 = CC14_317,
+                    intent_sen = CC355,
+                    intent_senx = CC355x,
+                    intent_gov = CC356,
+                    intent_rep = CC360,
+                    intent_repx = CC360x)
     }
     
     if (identical(cces_year, 2015L)) {
       tbl <- rename(tbl, CC350 = CC15_350) %>% 
-        rename(approval_rep = CC15_313a,
-               vote_pres_12 = CC15_315)
+        rename(approval_pres = CC15_312a,
+               approval_rep = CC15_313a,
+               approval_sen1 = CC15_313b,
+               approval_sen2 = CC15_313c,
+               approval_gov = CC15_312f,
+               voted_pres_12 = CC15_315)
     }
     
     if (identical(cces_year, 2016L)) {
@@ -142,9 +187,21 @@ stdName <- function(tbl){
                approval_sen1 = CC16_320g,
                approval_sen2 = CC16_320h,
                approval_gov = CC16_320d,
-               vote_pres_12 = CC16_326,
-               vote_pres_16 = CC16_410a,
-               vote_rep = CC16_367)
+               voted_pres_12 = CC16_326,
+               intent_trn = CC16_364,
+               intent_pres_16 = CC16_364c,
+               intent_pres_16x = CC16_364b,
+               intent_rep = CC16_367,
+               intent_repx = CC16_367x, # house early vote (already voted)
+               intent_sen = CC16_365,
+               intent_senx = CC16_365x,
+               intent_gov = CC16_366,
+               intent_govx = CC16_366x
+               # voted_rep = ,
+               # voted_sen = ,
+               # voted_gov = ,
+               # voted_pres_16 = ,
+               ) 
       
     }
     
@@ -168,26 +225,6 @@ stdName <- function(tbl){
     return(tbl)
 }
 
-# READ ------
-
-# 2012 and before (compiled by Stephen Pettigrew and others)
-ccp <- std_dv("data/source/cces/2006_2012_cumulative.dta", 
-                    guess_year = FALSE)
-
-
-
-# take the needed columns for 2013- 2016
-cc13 <- std_dv("data/source/cces/2013_cc.dta")
-cc14 <- std_dv("data/source/cces/2014_cc.dta")
-cc15 <- std_dv("data/source/cces/2015_cc.dta")
-cc16 <- std_dv("data/source/cces/2016_cc.dta")
-
-
-# helper data
-statecode <- read_csv("~/Dropbox/cces_rollcall/data/source/statecode.csv")
-
-
-
 # Start extracting variables -----
 # in list form
 ccs <- list(stdName(ccp), 
@@ -207,18 +244,26 @@ bryr <- findStack(ccs, birthyr, "numeric")
 state <- findStack(ccs, state, "character")
 cdid <- findStack(ccs, cdid, "numeric")
 
-pres08 <- findStack(ccs, vote_pres_08)
-pres12 <- findStack(ccs, vote_pres_12)
-pres16 <- findStack(ccs, vote_pres_16)
-# houvote <-
-# senvote <-
-# govvote <- 
+i_pres08 <- findStack(ccs, intent_pres_08)
+i_pres12 <- findStack(ccs, intent_pres_12)
+i_pres16 <- findStack(ccs, intent_pres_16)
+i_rep <- findStack(ccs, intent_rep)
+i_sen <- findStack(ccs, intent_sen)
+i_gov <- findStack(ccs, intent_gov)
+
+v_pres08 <- findStack(ccs, voted_pres_08)
+v_pres12 <- findStack(ccs, voted_pres_12)
+v_pres16 <- findStack(ccs, voted_pres_16)
+v_rep <- findStack(ccs, voted_rep)
+v_sen <- findStack(ccs, voted_sen)
+v_gov <- findStack(ccs, voted_gov)
 # 
 #   
 # presapv
-houapv <- findStack(ccs, approval_rep)
-# senapv <-
-# govapv <-
+apvrep <- findStack(ccs, approval_rep)
+apvsen1 <- findStack(ccs, approval_sen1)
+apvsen2 <- findStack(ccs, approval_sen2)
+apvgov <- findStack(ccs, approval_gov)
 
 # format state and CD ----
 stcd <- left_join(state, cdid) %>% 
@@ -236,10 +281,17 @@ ccc <- stcd %>%
   left_join(bryr) %>%
   left_join(race) %>%
   left_join(educ) %>% 
-  left_join(houapv) %>% 
+  left_join(apvrep) %>% 
+  left_join(apvsen1) %>% 
+  left_join(apvsen2) %>% 
+  left_join(apvgov) %>% 
   left_join(pres08) %>%
   left_join(pres12) %>%
-  left_join(pres16)
+  left_join(pres16) %>% 
+  left_join(voterep) %>% 
+  left_join(votesen) %>% 
+  left_join(votegov)
+  
 
 
 
