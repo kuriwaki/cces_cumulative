913168060de28c80cb9b1ebf81e9430ecc406ab6
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2017-07-08 11:09:06 +0900

    indent, fix data path, lint, rename

--- build/06_getMCdata.R
+++ build/06_getMCdata.R
@@ -1,9 +1,28 @@
-library(plyr)
-setwd("~/Dropbox/cces_cumulative/documents_CQ/congress 114 metadata")
-file_names = list.files(getwd())
-files = lapply(file_names,read_xlsx)
-colnames = c("Last","First","Middle","Suffix","Nickname","Born","Death","Sex","Position","Party","State","District","Start","End","Religion","Race","JobType1","JobType2","JobType3","JobType4","JobType5","Mil1","Mil2","Mil3")
-files <- lapply(files, setNames, colnames)
-df <- ldply(files, data.frame)
-df<-df[!(df$Last=="Last"),]
+library(readxl)
+
+# the working directory (i.e. the project directory)
+wd <- "~/Dropbox/cces_cumulative"
+
+# get file paths of all files in data/source/cq/mc_metadata
+file_names <- list.files(file.path(wd, "data/source/cq/mc_metadata"),
+                         full.names = TRUE,
+                         recursive = TRUE)
+
+# read files
+cq_raw <- lapply(file_names, read_excel)
+
+colnames <- c("Last","First","Middle","Suffix","Nickname",
+              "Born","Death",
+              "Sex",
+              "Position","Party",
+              "State","District",
+              "Start","End",
+              "Religion","Race",
+              "JobType1","JobType2","JobType3","JobType4","JobType5",
+              "Mil1","Mil2","Mil3")
+
+files <- lapply(cq_raw, setNames, colnames)
+
+df <- plyr::ldply(cq_raw, data.frame)
+df <- df[!(df$Last=="Last"),]
 df <- df[!is.na(df$First),]
