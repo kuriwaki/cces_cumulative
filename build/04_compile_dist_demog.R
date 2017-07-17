c3afe7aa1da3d353b90f4cd00d7ee8f532c7865c
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2017-07-17 14:40:12 -0400

    extract labels attriubute

--- build/04_compile_dist_demog.R
+++ build/04_compile_dist_demog.R
@@ -11,8 +11,34 @@ setwd("~/Dropbox/cces_cumulative")
 
 help(package = readr)
 help(package = haven)
-demcode111 <- read_tsv("data/source/censusCD_demographics/111/R11430177_SL500.txt")
 
-demcode_06_10 <- read_tsv("data/source/censusCD_demographics/ASC(5yr_2006_10)/R11430181_SL500.txt")
 
-demcode_11_15 <- read_tsv("data/source/censusCD_demographics/ASC(5yr_2011_15)/R11430939_SL500.txt")
+path <-"data/source/censusCD_demographics/ACS_5yr_2006_10/R11430181.dta"
+acs_06_10 <- read_dta(path)
+
+path1 <- "data/source/censusCD_demographics/ACS_5yr_2011_15/R11430939.dta"
+acs_11_15 <- read_dta(path1)
+
+path2 <- "data/source/censusCD_demographics/census_2010/R11430177_SL500.dta"
+cen_10 <- read_dta(path2)
+
+
+# table of variable and their labels
+
+# function to extract
+makeVlist <- function(dta) {
+  labels <- sapply(dta, function(x) attr(x, "label"))
+  
+  tibble(colname = names(labels),
+         description = labels)
+}
+
+
+# apply the function
+v_acs_06 <- makeVlist(acs_06_10)
+v_acs_11 <- makeVlist(acs_11_15)
+v_cen_10 <- makeVlist(cen_10)
+
+
+# see for example 
+View(v_cen_10)
