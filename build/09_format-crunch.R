4f0686d7043613632549f154fe74c7dc751f8ee6
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2017-12-20 13:02:46 -0500

    correct approval vars and add time

--- build/09_format-crunch.R
+++ build/09_format-crunch.R
@@ -18,16 +18,14 @@ if (upload_again) {
 login() # you need a login and password to complete this command
 
 
-
-
-
 # connect to data---------
-ds <- loadDataset("cumulative_2006_2016_dropbox.sav", project = "CCES Common Content")
+ds <- loadDataset("cumulative_2006_2016.sav", project = "CCES Common Content")
 
 
 # description for dataset
 description(ds) <- "This is an alpha version -- formatting incomplete and may contain errors."
-
+startDate(ds) <- "2006-10-06"
+endDate(ds) <- "2006-11-07"
 
 # variable type -----
 
@@ -41,6 +39,7 @@ type(ds$pid7) <- "categorical"
 # add question wording? ---------
 
 description(ds$year) <- "[Year of CCES Common Content]"
+description(ds$starttime) <- "[Pre-election wave start time]"
 description(ds$caseID) <- "[Case (Respondent) Idenfitier. Unique within year]"
 description(ds$weight) <- "[weights from pre-survey of each year]"
 description(ds$weight_cumulative) <- "[weights from pre-survey of each year, mulitiplied a constant within year to make years comparable]"
@@ -58,7 +57,7 @@ description(ds$pid7) <- "[branching based on pid3]"
 description(ds$approval_rep_char) <- "Do you approve of the way each is doing their job... [Pipe Incumbent Representative's Name and Party]"
 description(ds$approval_sen1_char) <- "Do you approve of the way each is doing their job... [Pipe Incumbent Senator's Name and Party]"
 description(ds$approval_sen2_char) <- "Do you approve of the way each is doing their job... [Pipe Incumbent Senator's Name and Party]"
-description(ds$approval_gov_char) <- "Do you approve of the way each is doing their job... [Pipe Incumbent Governor's Name and Party]"
+description(ds$approval_gov) <- "Do you approve of the way each is doing their job... [Pipe Incumbent Governor's Name and Party]"
 
 description(ds$voted_pres_16_char) <- "For whom did you vote for President of the United States?"
 description(ds$intent_pres_16_char) <- "Which candidate did you prefer for President of the United States?"
@@ -85,12 +84,14 @@ categories(ds$state) <- categories(ds$state)[st_order]
 # Variable Groups and ordering ------
 vn <- names(ds)
 
-ind_adm <- grep("year", vn)
+ind_adm <- grep("year|starttime", vn)
 ind_geo <- grep("CD|state", vn)
 ind_wgt <- grep("weight", vn)
 ind_dem <- grep("gender|birthyr|race|educ|pid", vn)
 
-ind_app <- grep("approval_.*char", vn)
+ind_app <- grep("approval_.*", vn)
+ind_app <- setdiff(ind_app, grep("_num$", vn)) # don't need numerics
+
 ind_pres <- setdiff(grep("pres.*char", vn), ind_app)
 
 ind_int <- setdiff(grep("intent.*char", vn), ind_pres)
@@ -103,14 +104,12 @@ ordering(ds) <- VariableOrder(
   VariableGroup("Administration", ds[ind_adm]),
   VariableGroup("Geography", ds[ind_geo]),
   VariableGroup("Demographics", ds[ind_dem]),
-  VariableGroup("Approval", ds[ind_app]),
   VariableGroup("Presidential Intent and Vote", ds[ind_pres]),
   VariableGroup("Vote Intent (Other Offices)", ds[ind_int]),
   VariableGroup("Voted (Other Offices)", ds[ind_vtd]),
+  VariableGroup("Approval", ds[ind_app]),
   VariableGroup("Weights", ds[ind_wgt]),
   VariableGroup("Other", ds[ind_other])
 )
 
 
-
-logout()
