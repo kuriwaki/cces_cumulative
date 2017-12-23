54201767f8bdb6229d2e941a3c3fc98b0fe1c1f2
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2017-12-23 01:14:18 -0500

    format and rename objects

--- build/13_build-DIME.R
+++ build/13_build-DIME.R
@@ -1,13 +1,33 @@
-#badchart is original long list of all dime recipients from 1979-2014
-#goodchart is badchart with only candidates
-#minigoodchart is goodchart with only relevant columns
-#minigoodchart 2 is minigoodchart without blank rows
-#minigoodchartfinal is minigoodchart 2 without duplicate rows
-badchart <- read.csv("~/Dropbox/cces_cumulative/data/source/dime/dime_recipients_all_1979_2014.csv")
-badchart[!grepl("comm",badchart$bonica.rid),] -> goodchart
-minigoodchart <- data_frame(FEC = goodchart$FEC.ID, ICPSR = goodchart$ICPSR2, seat = goodchart$seat)
-minigoodchart2 <- minigoodchart[!is.na(minigoodchart$FEC),]
-minigoodchartfinal <- distinct(minigoodchart2,FEC,ICPSR, seat)
-#strip away letters from icpsr
-minigoodchartfinal$ICPSR <- gsub("[A-z]+","",minigoodchartfinal$ICPSR)
-minigoodchartfinal$ICPSR <- as.numeric(minigoodchartfinal$ICPSR)
+library(tidyverse)
+
+# dime_raw is original long list of all dime recipients from 1979-2014
+# dime_cand is dime_raw with only candidates
+# dime_cols is dime_cand with only relevant columns
+# dime_full is dime_cols without blank rows
+# df is minigoodchart 2 without duplicate rows
+
+dime_raw <- read_csv("data/source/dime/dime_recipients_all_1979_2014.csv")
+
+dime_cand <- dime_raw %>% 
+  filter(!grepl("comm", bonica.rid))
+
+dime_cols <- dime_cand %>% 
+  select(FEC = FEC.ID,
+         ICPSR = ICPSR2, 
+         seat = seat)
+
+dime_full <- dime_cols %>%
+  filter(!is.na(FEC))
+
+df <- dime_full %>% 
+  distinct(FEC, ICPSR, seat)
+
+
+# strip away letters from icpsr
+df <-  df %>%
+  mutate(ICPSR  = gsub("[A-z]+", "", ICPSR)) %>%
+  mutate(ICPSR = as.integer(ICPSR))
+
+
+# save
+saveRDS(df, "data/output/03_contextual/dime_fmt.Rds")
