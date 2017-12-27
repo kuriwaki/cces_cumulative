dea07c62c8bb87f49c66912c5193598a27527b82
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2017-12-27 12:45:03 -0500

    use candidate fec id, do more formatting of districts and states to match non-incumbents, merge into incumbents

--- build/13_build-DIME.R
+++ build/13_build-DIME.R
@@ -1,36 +1,60 @@
 library(tidyverse)
 
-# dime_raw is original long list of all dime recipients from 1979-2014
-# dime_cand is dime_raw with only candidates
-# dime_cols is dime_cand with only relevant columns
-# dime_full is dime_cols without blank rows
-# df is minigoodchart 2 without duplicate rows
 
 dime_raw <- read_csv("data/source/dime/dime_recipients_all_1979_2014.csv")
 
+# filter only candidates
 dime_cand <- dime_raw %>%
-  filter(!grepl("comm", bonica.rid))
+  filter(recipient.type == "cand")
 
 dime_cols <- dime_cand %>%
   select(
-    FEC = FEC.ID,
-    ICPSR = ICPSR2,
-    seat = seat
+    elec = fecyear,
+    fec = Cand.ID,
+    icpsr = ICPSR2, # ICPSR has the year of donation attached to it
+    office_sought = seat,
+    party,
+    st = state, 
+    dist = district,
+    incumb = Incum.Chall,
+    name,
+    namelast = lname,
+    namefirstm = ffname
+  ) %>% 
+  mutate(
+    name = toupper(name),
+    namelast = toupper(namelast)
   )
 
 dime_full <- dime_cols %>%
-  filter(!is.na(FEC))
+  filter(!is.na(fec))
 
-df <- dime_full %>%
-  distinct(FEC, ICPSR, seat)
+df_distinct <- dime_full %>% distinct()
+  
+
+# keep ICPSR as ICPSR, drop FEC (in DIME data, candidates who have never been 
+# incumbents and thus have no FEC are just given the FEC code).
+
+# for those that remain, change to integer
+
+df_fec <- df_distinct %>% 
+  mutate(icpsr = replace(icpsr, icpsr == fec, "")) %>%
+  mutate(icpsr_num = as.integer(icpsr)) # a real ICPSR (of incumbents) should have no letters in the first place 
+
+sample_n(filter(df_fec, grepl("federal", office_sought)), 10)
 
 
-# strip away letters from icpsr
-df <- df %>%
-  mutate(ICPSR = gsub("[A-z]+", "", ICPSR)) %>%
-  mutate(ICPSR = as.integer(ICPSR)) %>%
-  rename(icpsr = ICPSR)
+# modify dist
+# only keep dist for house and state leg
+
+regex_dist_offices <- "(federal:house|state:lower|state:upper)"
+
+df <-  df_fec %>% 
+  mutate(dist = replace(dist, !grepl(regex_dist_offices, office_sought), "")) %>%
+  mutate(dist = gsub("[A-Z-]+", "", dist)) %>% # can strip state for these offices
+  mutate(dist = as.integer(dist)) %>%
+  mutate(st = toupper(st))  # formatting corrections
 
 
 # save
-saveRDS(df, "data/output/03_contextual/dime_fmt.Rds")
+saveRDS(df, "data/output/03_contextual/fec_fmt.Rds")
--- build/14_join-MCs.R
+++ build/14_join-MCs.R
@@ -8,21 +8,22 @@ vvS <- readRDS("data/output/03_contextual/voteview_S_key.Rds")
 
 cq <- readRDS("data/output/03_contextual/cq_profiles.Rds")
 
-dime <- readRDS("data/output/03_contextual/dime_fmt.Rds")
+fec <- readRDS("data/output/03_contextual/fec_fmt.Rds")
 
 
 # join Voteview and FEC list (with appropriate offices) ----
 vv_fec_H <- left_join(
   vvH,
-  filter(dime, seat == "federal:house"),
-  by = "icpsr"
+  filter(fec, office_sought == "federal:house") %>% distinct(icpsr_num, fec),
+  by = c("icpsr" = "icpsr_num")
 )
 
 vv_fec_S <- left_join(
   vvS,
-  filter(dime, seat == "federal:senate"),
-  by = "icpsr"
+  filter(fec, office_sought == "federal:senate") %>% distinct(icpsr_num, fec),
+  by = c("icpsr" = "icpsr_num")
 )
+
 # join CQ ---
 df_H <- left_join(vv_fec_H, cq, by = c("namelast", "chamber", "st", "dist", "congress"))
 df_S <- left_join(vv_fec_S, cq, by = c("namelast", "chamber", "st", "congress"))
