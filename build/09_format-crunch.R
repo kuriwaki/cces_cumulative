9f01a236fda6c4d7677590b4d8f5ba6df901edf3
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2018-01-05 19:15:55 -0500

    apply the metadata in grid form

--- build/09_format-crunch.R
+++ build/09_format-crunch.R
@@ -25,52 +25,60 @@ description(ds) <- "This is a working version -- formatting incomplete and may c
 startDate(ds) <- "2006-10-06"
 endDate(ds) <- "2006-11-07"
 
-# variable type -----
 
+
+
+
+# add metadata ---------
+
+ccc_meta <- tribble(
+  ~alias, ~type, ~name, ~description,
+  "year",                "categorical",  "Year", "[Year of CCES Common Content]",
+  "starttime",           "datetime",    "Start Time", "[Pre-election wave start time]",
+  "caseID",              "text",        "Identifier", "[Case (Respondent) Idenfitier. Unique within year]",
+  "weight",              "numeric",     "Common content weight", "[weights from pre-survey of each year]",
+  "weight_cumulative",   "numeric",     "Survey Weight (Cumulative)", "[weight variable with simple adjustment: mulitiplied a constant within year to make years comparable]",
+  "CD",                  "categorical", "Congressional District", "[Congressional District (Imputed from input zipcode)]",
+  "state",               "categorical", "State", "[State (Imputed from input zipcode)]",
+  "gender",              "categorical", "Gender", "Are you male or female?",
+  "birthyr",             "numeric",     "Year of Birth", "In what year were you born?",
+  "educ",                "categorical", "Education", "What is the highest level of education you have completed?",
+  "race",                "categorical", "Race", "What racial or ethnic group best describes you?",
+  "pid3",                "categorical", "Partisan Identity", "Generally speaking, do you think of yourself as a ...?",
+  "pid7",                "categorical", "Partisan Identity (7 point)", "[based on branching from Partisan Identity question]",
+  "approval_pres",       "categorical", "President Approval", "Do you approve of the way each is doing their job... [Pipe Incumbent President]",
+  "approval_rep_char",   "categorical", "House Representative Approval", "Do you approve of the way each is doing their job... [Pipe Incumbent Representative's Name and Party]",
+  "approval_sen1_char",  "categorical", "Senator 1 Approval", "Do you approve of the way each is doing their job... [Pipe Incumbent Senator's Name and Party]",
+  "approval_sen2_char",  "categorical", "Senator 2 Approval", "Do you approve of the way each is doing their job... [Pipe Incumbent Senator's Name and Party]",
+  "approval_gov",        "categorical", "Governor Approval", "Do you approve of the way each is doing their job... [Pipe Incumbent Governor's Name and Party]",
+  "voted_pres_16_char",  "categorical", "2016 President vote choice", "For whom did you vote for President of the United States? [post, with early voters in pre coalesced]",
+  "intent_pres_16_char", "categorical", "2016 President preference",  "Which candidate did you prefer for President of the United States?",
+  "voted_pres_12_char",  "categorical", "2012 President vote choice", "[2012 wording] For whom did you vote for President of the United States? [2016 wording]: In 2012, who did you vote for in the election for President? [see appendix for wording in all years]",
+  "intent_pres_12_char", "categorical", "2012 President preference", "In the race for President of the United States, who do you prefer?",
+  "voted_pres_08_char",  "categorical", "2008 President vote choice", "[2008 wording] For which candidate for President of the United States did you vote?  [see appendix for wording in all years]",
+  "intent_pres_08_char", "categorical", "2008 President preference", "For which candidate for President of the United States would you vote?",
+  "intent_rep_char",     "categorical", "House preference", "In the general election for U.S. House of Representatives in your area, who do you prefer?",
+  "voted_rep_char",      "categorical", "House vote choice", "For whom did you vote for U.S. House?",
+  "intent_sen_char",     "categorical", "Senate preference", "In the race for U.S. Senator in your state, who do you prefer?",
+  "voted_sen_char",      "categorical", "Senate vote choice", "For whom did you vote for U.S. Senator?",
+  "intent_gov_char",     "categorical", "Governor preference", "In the race for Governor in your state, who do you prefer?",
+  "voted_gov_char",      "categorical", "Governor vote choice", "For whom did you vote for Governor?"
+  )
+
+# appply
+
+lapply(ds, function(v){
+  name(v) <-        ccc_meta$name[ccc_meta$alias == alias(v)]
+  description(v) <- ccc_meta$description[ccc_meta$alias == alias(v)]
+})
+
+
+# apply weights ---
+weight(ds) <- ds$weight_cumulative
+
+# change look  -----
 type(ds$year) <- "categorical"
 names(categories(ds$year)) <- as.character(2006:2016)
-
-type(ds$pid7) <- "categorical"
-
-
-
-# add question wording? ---------
-
-description(ds$year) <- "[Year of CCES Common Content]"
-description(ds$starttime) <- "[Pre-election wave start time]"
-description(ds$caseID) <- "[Case (Respondent) Idenfitier. Unique within year]"
-description(ds$weight) <- "[weights from pre-survey of each year]"
-description(ds$weight_cumulative) <- "[weights from pre-survey of each year, mulitiplied a constant within year to make years comparable]"
-description(ds$CD) <- "[Congressional District (Imputed from input zipcode)]"
-description(ds$state) <- "[State (Imputed from input zipcode)]"
-
-description(ds$gender) <- "Are you male or female?"
-description(ds$birthyr) <- "In what year were you born?"
-description(ds$educ) <- "What is the highest level of education you have completed?"
-description(ds$race) <- "What racial or ethnic group best describes you?"
-
-description(ds$pid3) <- "Generally speaking, do you think of yourself as a ...?"
-description(ds$pid7) <- "[branching based on pid3]"
-
-description(ds$approval_rep_char) <- "Do you approve of the way each is doing their job... [Pipe Incumbent Representative's Name and Party]"
-description(ds$approval_sen1_char) <- "Do you approve of the way each is doing their job... [Pipe Incumbent Senator's Name and Party]"
-description(ds$approval_sen2_char) <- "Do you approve of the way each is doing their job... [Pipe Incumbent Senator's Name and Party]"
-description(ds$approval_gov) <- "Do you approve of the way each is doing their job... [Pipe Incumbent Governor's Name and Party]"
-
-description(ds$voted_pres_16_char) <- "For whom did you vote for President of the United States?"
-description(ds$intent_pres_16_char) <- "Which candidate did you prefer for President of the United States?"
-
-description(ds$voted_pres_12_char) <- "2012 wording: For whom did you vote for President of the United States? 2016 wording: Which candidate did you prefer for President of the United States? (see appendix for wording in all years)"
-description(ds$intent_pres_12_char) <- "In the race for President of the United States, who do you prefer?"
-
-description(ds$intent_rep_char) <- "In the general election for U.S. House of Representatives in your area, who do you prefer?"
-description(ds$voted_rep_char) <- "For whom did you vote for U.S. House?"
-
-description(ds$intent_sen_char) <- "In the race for U.S. Senator in your state, who do you prefer?"
-description(ds$voted_sen_char) <- "For whom did you vote for U.S. Senator?"
-
-description(ds$intent_gov_char) <- "In the race for Governor in your state, who do you prefer?"
-description(ds$voted_gov_char) <- "For whom did you vote for Governor?"
 
 
 # ordering of categories ----
