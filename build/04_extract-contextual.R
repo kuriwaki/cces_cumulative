795437c36c88ebe86178b046649b5a472b737bca
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2018-01-07 01:09:52 -0500

    try variable matrix

--- build/04_extract-contextual.R
+++ build/04_extract-contextual.R
@@ -8,15 +8,47 @@ inc_S <- readRDS("data/output/03_contextual/incumbents_S.Rds")
 statecode <- read_csv("data/source/statecode.csv")
 
 fmt_geo <- function(tbl) {
-  tbl %>%
-  mutate(state = as.character(as_factor(state)),
-         cdid = as.integer(cdid),
-         dist = as.integer(cdid)) %>%
-  left_join(select(statecode, state, st), by = "state")
+  if (class(tbl$state) == "character") {
+    fmt <- tbl %>% 
+      mutate(cdid = as.integer(cdid),
+             dist = as.integer(cdid))
+  }
+  
+  if (class(tbl$state) == "labelled") {
+    fmt <- tbl %>%
+      mutate(state = as.character(as_factor(state)),
+             cdid = as.integer(cdid),
+             dist = as.integer(cdid))
+  }
+  
+  left_join(fmt, select(statecode, state, st), by = "state") %>%
+    mutate_at(vars(matches("_can")), function(x) replace(x, x == "__NA__" | x == "", NA)) %>% # make NA if empty or "_NA_"
+    select(year, st, dist, everything())
 }
 
 
 sel_common <- c("year", "cong_inc", "cong_up", "caseID")
+
+# do something like 
+master <- 
+  tribble(~name,      ~`2006`, ~`2007`,     ~`2008`, ~`2009`, ~`2010`, ~`2011`, ~`2012`,            ~`2013`,            ~`2014`,            ~`2015`,            ~`2016`,
+          "state",    "v1002", "inputstate", "V206",  "v259",  "V206",  "V206", "inputstate",       "inputstate",       "inputstate",       "inputstate",       "inputstate",
+          "cdid",     "v1003", "cdidnum",    "V250",  "v264",  "V276",  "V276", "cdid",             "cdid113",          "cdid",             "cdid113",          "cdid113",
+          "gov_inc",  "v5019", "govname",    "V508",  "v608",  "V529",  "V529", "CurrentGovName",   "CurrentGovName",   "CurrentGovName",   "CurrentGovName",   "CurrentGovName",
+          "gov_can1", "v5009", NA,           "V501",  NA,      "V564",  NA,     "GovCand1Name",     NA,                 "GovCand1Name",     NA,                 "GovCand1Name",
+          "gov_can2", "v5011", NA,           "V502",  NA,      "V567",  NA,     "GovCand2Name",     NA,                 "GovCand2Name",     NA,                 "GovCand2Name",
+          "hou_inc",  "v5013", "repname",    "V527",  "v627",  "V501",  "V521", "CurrentHouseName", "CurrentHouseName", "CurrentHouseName", "CurrentHouseName", "CurrentHouseName",
+          "hou_can1", "v5001", NA,           "V518",  "v618",  "V533",  "V533", "HouseCand1Name",   NA,                 "HouseCand1Name",   NA,                 "HouseCand1Name",
+          "hou_can2", "v5003", NA,           "V519",  "v619",  "V536",  "V536", "HouseCand2Name",   NA,                 "HouseCand2Name",   NA,                 "HouseCand2Name",
+          "sen1_inc", "v5015", "sen1name",   "V551",  "v651",  "V513",  "V501", "CurrentSen1Name",  "CurrentSen1Name",  "CurrentSen1Name",  "CurrentSen1Name",  "CurrentSen1Name",
+          "sen2_inc", "v5017", "sen2name",   "V552",  "v652",  "V521",  "V513", "CurrentSen2Name",  "CurrentSen2Name",  "CurrentSen2Name",  "CurrentSen2Name",  "CurrentSen2Name",
+          "sen_can1", "v5005", NA,           "V553",  "v653",  "V548",  NA,     "SenCand1Name",     NA,                 "SenCand1Name",     NA,                 "SenCand1Name",
+          "sen_can2", "v5007", NA,           "V555",  "v655",  "V551",  NA,     "SenCand2Name",     NA,                 "SenCand2Name",     NA,                 "SenCand2Name"
+          )
+
+
+
+
 
 # 2006
 ctx06 <- 
@@ -24,34 +56,18 @@ ctx06 <-
   mutate(cong_inc = 109L,
          cong_up = 110L) %>%
   select(!!sel_common,
-         st = v1002,
-         cdid = v1003,
-         gov_inc = v5019,
-         gov_dem = v5009,
-         gov_rep = v5011,
-         hou_rep = v5013,
-         hou_can1 = v5001,
-         hou_can2 = v5003,
-         sen1_inc = v5015,
-         sen2_inc = v5017,
-         sen_can1 = v5005,
-         sen_can2 = v5007) %>% 
-  left_join(select(statecode, state, st), by = "st") %>% 
+         ) %>% 
+  left_join(select = statecode, state, st), by = "st") %>% 
   select(-st) %>%
   fmt_geo()
+
 
 # 2007
 ctx07 <-
   cc07 %>% 
   mutate(cong_inc = 110L,
          cong_up = 111L) %>%
-  select(!!sel_common,
-         state = inputstate,
-         cdid = cdid_num,
-         gov_inc = govname, # verify with Spitzer in NY 
-         hou_inc = repname,
-         sen_inc1 = sen1name,
-         sen_inc2 = sen2name) %>% 
+  select(!!sel_common) %>% 
   fmt_geo()
 
 
@@ -61,20 +77,8 @@ ctx08 <-
   mutate(cong_inc = 110L,
          cong_up = 111L) %>%
   select(!!sel_common, 
-         state = V206,
-         cdid = V250,
          reszip = V202, 
-         regzip = V204,
-         gov_inc = V508,
-         gov_can1 = V501,
-         gov_can2 = V502,
-         hou_inc = V527,
-         hou_can1 = V518,
-         hou_can2 = V519,
-         sen1_inc = V551,
-         sen2_inc = V552,
-         sen_can1 = V553,
-         sen_can2 = V555) %>% 
+         regzip = V204,) %>% 
   fmt_geo()
 
 # 2009
@@ -92,16 +96,6 @@ ctx09 <-
   mutate(cong_inc = 111L,
          cong_up = 112L) %>%
   select(!!sel_common,
-         state = v259,
-         cdid = v264,
-         gov_inc = v608,
-         hou_inc = v627,
-         hou_can1 = v618, # labelled as R/D in 2009
-         hou_can2 = v619,
-         sen1_inc = v651,
-         sen2_inc = v652,
-         sen_can1 = v653,
-         sen_can2 = v655)  %>% 
   mutate(state = labelled(zap_labels(state), newlabs)) %>%
   fmt_geo() 
   
@@ -114,20 +108,8 @@ ctx10 <-
   mutate(cong_inc = 111L,
          cong_up = 112L) %>%
   select(!!sel_common,
-         state = V206,
-         cdid = V276,
          reszip = V202, 
-         regzip = V205,
-         gov_inc = V529,
-         gov_can1 = V564,
-         gov_can2 = V567,
-         hou_inc = V501,
-         hou_can1 = V533,
-         hou_can2 = V536,
-         sen1_inc = V513,
-         sen2_inc = V521,
-         sen_can1 = V548,
-         sen_can2 = V551) %>% 
+         regzip = V205) %>% 
   fmt_geo()
 
 # 2011
@@ -136,15 +118,7 @@ ctx11 <-
   mutate(cong_inc = 112L,
          cong_up = 113L) %>% 
   select(!!sel_common,
-         state = V206,
-         cdid = V276, 
-         gov_inc = V529,
-         hou_inc = V521,
-         hou_can1 = V533, # 2010 vote, D/R
-         hou_can2 = V536,
-         sen1_inc = V501,
-         sen2_inc = V513) %>% 
-  fmt_geo()
+         hou_can1 = , # 2010 vote, D/R
 
 # 2012
 ctx12 <- 
@@ -152,19 +126,7 @@ ctx12 <-
   mutate(cong_inc = 112L,
          cong_up = 113L) %>% 
   select(!!sel_common,
-         state = inputstate,
-         cdid = cdid,
          cdid_up = cdid113,
-         gov_inc = CurrentGovName,
-         gov_can1 = GovCand1Name,
-         gov_can2 = GovCand2Name,
-         hou_inc = CurrentHouseName,
-         hou_can1 = HouseCand1Name,
-         hou_can2 = HouseCand2Name,
-         sen_inc1 = CurrentSen1Name,
-         sen_inc2 = CurrentSen2Name,
-         sen_can1 = SenCand1Name,
-         sen_can2 = SenCand2Name) %>% 
   fmt_geo()
 
 # 2013 
@@ -175,10 +137,7 @@ ctx13 <-
   select(!!sel_common,
          state = inputstate,
          cdid = cdid113,
-         gov_inc = CurrentGovName, # NJ and VA Gov
-         hou_inc = CurrentHouseName,
-         sen_inc1 = CurrentSen1Name,
-         sen_inc2 = CurrentSen2Name) %>% 
+         gov_inc = CurrentGovName) # NJ and VA Gov
   fmt_geo()
 
 
@@ -188,18 +147,6 @@ ctx14 <-
   mutate(cong_inc = 113L,
          cong_up = 114L) %>% 
   select(!!sel_common,
-         state = inputstate,
-         cdid,
-         gov_inc = CurrentGovName,
-         gov_can1 = GovCand1Name,
-         gov_can2 = GovCand2Name,
-         hou_inc = CurrentHouseName,
-         hou_can1 = HouseCand1Name,
-         hou_can2 = HouseCand2Name,
-         sen_inc1 = CurrentSen1Name,
-         sen_inc2 = CurrentSen2Name,
-         sen_can1 = SenCand1Name,
-         sen_can2 = SenCand2Name) %>% 
   fmt_geo()
 
 # 2015 
@@ -208,12 +155,7 @@ ctx15 <-
   mutate(cong_inc = 114L,
          cong_up = 115L) %>% 
   select(!!sel_common,
-         state = inputstate,
-         cdid = cdid,
          gov_inc = CurrentGovName, # KY, LA, MS Gov
-         hou_inc = CurrentHouseName,
-         sen_inc1 = CurrentSen1Name,
-         sen_inc2 = CurrentSen2Name) %>% 
   fmt_geo()
 
 
@@ -224,19 +166,9 @@ ctx16 <-
   mutate(cong_inc = 114L,
          cong_up = 115L) %>%
   select(!!sel_common,
-         state = inputstate,
-         cdid = cdid113,
          cdid_up = cdid115,
-         gov_inc = CurrentGovName,
-         gov_can1 = GovCand1Name,
-         gov_can2 = GovCand2Name,
-         hou_inc = CurrentHouseName,
-         hou_can1 = HouseCand1Name,
-         hou_can2 = HouseCand2Name,
-         sen_inc1 = CurrentSen1Name,
-         sen_inc2 = CurrentSen2Name,
-         sen_can1 = SenCand1Name,
-         sen_can2 = SenCand2Name) %>% 
+         state = "inputstate",
+         cdid = "cdid113",
   fmt_geo()
 
 
@@ -258,11 +190,7 @@ key <- bind_rows(
 )
 
 
-# do something like 
 
-master <- 
-  tribble(~name, ~`2006`, ~`2007`, ~`2016`,
-          "state", "v1002", "inputstate", "inputstate")
 
 
 
