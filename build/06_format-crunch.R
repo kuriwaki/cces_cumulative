a066fab093132001fdb9e6c4dec81f75c1986c22
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2018-01-10 23:01:30 -0500

    separate out the variable metatable

--- build/06_format-crunch.R
+++ build/06_format-crunch.R
@@ -2,6 +2,8 @@ library(crunch)
 library(dplyr)
 
 
+ccc_meta <- readRDS("data/output/02_questions/cumulative_vartable.Rds")
+
 # Start up crunch -------
 login() # you need a login and password to complete this command
 
@@ -16,84 +18,6 @@ endDate(ds) <- "2006-11-07"
 
 
 # add metadata ---------
-
-ccc_meta <- tribble(
-  ~alias, ~type, ~name, ~description,
-  "year",                "categorical", "Year", "[Year of CCES Common Content]",
-  "starttime",           "datetime",    "Start Time", "[Pre-election wave start time]",
-  "caseID",              "text",        "Identifier", "[Case (Respondent) Idenfitier. Unique within year]",
-  "weight",              "numeric",     "Common content weight", "[weights from pre-survey of each year]",
-  "weight_cumulative",   "numeric",     "Survey Weight (Cumulative)", "[weight variable with simple adjustment: mulitiplied a constant within year to make years comparable]",
-  "CD",                  "categorical", "Congressional District", "[Current Congressional District (Imputed from input zipcode)]",
-  "cdid",                "categorical", "Congressional District Number", "[Congressional District (Imputed from input zipcode)]",
-  "state",               "categorical", "State", "[State (Imputed from input zipcode)]",
-  "st",                  "categorical", "State Abbreviation", "[State (Imputed from input zipcode)]",
-  "zipcode",             "text",        "Zipcode of Residence", "So that we can ask you about the news and events in your area, in what zip code do you currently reside?",
-  "countyFIPS",          "text",        "County of Residence", "[]",
-  "gender",              "categorical", "Gender", "Are you male or female?",
-  "birthyr",             "numeric",     "Year of Birth", "In what year were you born?",
-  "age",                 "numeric",     "Age", "[Approximate; computed from the year of survey minus Year of Birth]",
-  "educ",                "categorical", "Education", "What is the highest level of education you have completed?",
-  "race",                "categorical", "Race", "What racial or ethnic group best describes you?",
-  "pid3",                "categorical", "Partisan Identity", "Generally speaking, do you think of yourself as a ...?",
-  "pid7",                "categorical", "Partisan Identity (7 point)", "[based on branching from Partisan Identity question]",
-  "approval_pres",       "categorical", "President Approval", "Do you approve of the way each is doing their job... [Pipe Incumbent President]",
-  "approval_rep",   "categorical", "House Representative Approval", "Do you approve of the way each is doing their job... [Pipe Incumbent Representative's Name and Party]",
-  "approval_sen1",  "categorical", "Senator 1 Approval", "Do you approve of the way each is doing their job... [Pipe Incumbent Senator's Name and Party]",
-  "approval_sen2",  "categorical", "Senator 2 Approval", "Do you approve of the way each is doing their job... [Pipe Incumbent Senator's Name and Party]",
-  "approval_gov",        "categorical", "Governor Approval", "Do you approve of the way each is doing their job... [Pipe Incumbent Governor's Name and Party]",
-  "economy_retro",       "categorical", "Retrospective Economy", "OVER THE PAST YEAR the nation's economy has ...?",
-  "vv_regstatus",        "categorical", "Validated Registration Status", "[Validation Results. Missing if validation was not conducted in the year. Categories are aggregated. Both Matched-not registered and unmatched are labelled as a no record.]",
-  "vv_party_gen",        "categorical", "Validated Registered Party", "[Validation Results.]",
-  "vv_party_prm",        "categorical", "Validated Registered Primary Party", "[Validation Results. All vote methods (polling, mail, early, unknown, etc..) are aggregated as a vote.]",
-  "vv_turnout_gvm",      "categorical", "Validated Turnout General Election", "[Validation Results. All vote methods (polling, mail, early, unknown, etc..) are aggregated as a vote.]",
-  "vv_turnout_pvm",      "categorical", "Validated Turnout Primary Election (Congressional)", "[Validation Results]",
-  "voted_pres_16",  "categorical", "2016 President vote choice", "For whom did you vote for President of the United States? [post, with early voters in pre coalesced]",
-  "intent_pres_16", "categorical", "2016 President preference",  "Which candidate did you prefer for President of the United States?",
-  "voted_pres_12",  "categorical", "2012 President vote choice", "[2012 wording] For whom did you vote for President of the United States? [2016 wording]: In 2012, who did you vote for in the election for President? [see appendix for wording in all years]",
-  "intent_pres_12", "categorical", "2012 President preference", "In the race for President of the United States, who do you prefer?",
-  "voted_pres_08",  "categorical", "2008 President vote choice", "[2008 wording] For which candidate for President of the United States did you vote?  [see appendix for wording in all years]",
-  "intent_pres_08", "categorical", "2008 President preference", "For which candidate for President of the United States would you vote?",
-  "approval_rep_num",    "categorical", "approval_rep_num", "[Response values (may be specific to year)]",
-  "approval_sen1_num",   "categorical", "approval_sen1_num", "[Response values (may be specific to year)]",
-  "approval_sen2_num",   "categorical", "approval_sen2_num", "[Response values (may be specific to year)]",
-  "voted_pres_16_num",   "categorical", "voted_pres_16_num", "[Response values (may be specific to year)]",
-  "intent_pres_16_num",  "categorical", "intent_pres_16_num", "[Response values (may be specific to year)]",
-  "voted_pres_12_num",   "categorical", "voted_pres_12_num", "[Response values (may be specific to year)]",
-  "intent_pres_12_num",  "categorical", "intent_pres_12_num", "[Response values (may be specific to year)]",
-  "voted_pres_08_num",   "categorical", "voted_pres_08_num", "[Response values (may be specific to year)]",
-  "intent_pres_08_num",  "categorical", "intent_pres_08_num", "[Response values (may be specific to year)]",
-  "intent_rep",          "categorical", "House preference", "In the general election for U.S. House of Representatives in your area, who do you prefer?",
-  "intent_sen",          "categorical", "Senate preference", "In the race for U.S. Senator in your state, who do you prefer?",
-  "intent_gov",          "categorical", "Governor preference", "In the race for Governor in your state, who do you prefer?",
-  "voted_rep",           "categorical", "House vote choice", "For whom did you vote for U.S. House?",
-  "voted_sen",           "categorical", "Senate vote choice", "For whom did you vote for U.S. Senator?",
-  "voted_gov",           "categorical", "Governor vote choice", "For whom did you vote for Governor?",
-  "intent_rep_chosen",   "text",        "House preference (shown)", "[Text chosen by respondent]",
-  "intent_sen_chosen",   "text",        "Senate preference (shown)", "[Text chosen by respondent]",
-  "intent_gov_chosen",   "text",        "Governor preference (shown)", "[Text chosen by respondent]",
-  "voted_rep_chosen",    "text",        "House vote choice (shown)", "[Text chosen by respondent]",
-  "voted_sen_chosen",    "text",        "Senate vote choice (shown)", "[Text chosen by respondent]",
-  "voted_gov_chosen",    "text",        "Governor vote choice (shown)", "[Text chosen by respondent]",
-  "intent_rep_fec",      "text",        "ID for House preference",     "[FEC ID]",
-  "intent_sen_fec",      "text",        "ID for Senate preference",    "[FEC ID]",
-  "intent_gov_fec",      "text",        "ID for Governor preference",  "[FEC ID]",
-  "voted_rep_fec",       "text",        "ID for House vote choice",    "[FEC ID]",
-  "voted_sen_fec",       "text",        "ID for Senate vote choice",   "[FEC ID]",
-  "voted_gov_fec",       "text",        "ID for Governor vote choice", "[FEC ID]",
-  "hou_inc",             "text",        "Current House Rep", "",
-  "sen1_inc",            "text",        "Current Senator 1", "",
-  "sen2_inc",            "text",        "Current Senator 2", "",
-  "gov_inc",             "text",        "Current Governor", "",
-  "hou_icpsr",           "text",        "ID for current House Rep", "[ICPSR / NOMINATE]",
-  "sen1_icpsr",          "text",        "ID for current Senator 1", "[ICPSR / NOMINATE]",
-  "sen2_icpsr",          "text",        "ID for current Senator 2", "[ICPSR / NOMINATE]",
-  "gov_fec",             "text",        "ID for current Governor", "[FEC ID]"
-  )
-
-# need to be unique
-stopifnot(n_distinct(ccc_meta$alias) == nrow(ccc_meta))
-stopifnot(n_distinct(ccc_meta$name) == nrow(ccc_meta))
 
 # appply the name and variable
 lapply(ds, function(v){
