9a64dcbcc85747e5836523cb35cbeda3959f1c96
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2018-06-18 22:53:27 -0400

    fork 2016 common content vv and prepare replacement columns

--- 08_combine_crunch.R
+++ 08_combine_crunch.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(crunch)
 library(dplyr)
 library(haven)
@@ -6,12 +7,31 @@ library(haven)
 login()
 
 
-ds16 <- loadDataset("CCES Cumulative Common 2016") # old dataset, 2006 - 2016
-ds17 <- loadDataset("CCES Cumulative Common 2017") # new dataset, only 2017 
+if(FALSE){
+  
+  ds16 <- loadDataset("CCES Cumulative Common 2016") # old dataset, 2006 - 2016
+  ds17 <- loadDataset("CCES Cumulative Common 2017") # new dataset, only 2017 
+  
+  ds16_17 <- appendDataset(ds16, ds17)
+  compareDatasets(ds16, ds17)
+  
+  # something wrong with variable number 6?
+  ds16[[6]]
+  ds17[[6]]
+  
+}
 
-ds16_17 <- appendDataset(ds16, ds17)
-compareDatasets(ds16, ds17)
+# fix 2016 cumulative
 
-# something wrong with variable number 6?
-ds16[[6]]
-ds17[[6]]
+# upload 2016
+cc16 <- read_dta("~/Dropbox/CCES_SDA/2016/data/Common/CCES16_Common_OUTPUT_Feb2018_VV.dta") %>% 
+  select(V101, matches("weight"), matches("^CL"))
+write_sav(cc16, "data/output/01_responses/cc16_temp.sav")
+newDataset("https://www.dropbox.com/s/fnv175o976rzxil/cc16_temp.sav?dl=0", "CCES 2016 Jan 2018")
+
+insert <- loadDataset("CCES 2016 Jan 2018")
+
+
+
+old16 <- loadDataset("CCES 2016 Common Vote Validated", project = "CCES")
+old16_fork <- forkDataset(old16)
