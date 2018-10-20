764a23d3ab47644653b8c538cb1c1d61f14c4f5e
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2018-10-20 14:26:59 -0400

    format and get age in

--- 09_export_past.R
+++ 09_export_past.R
@@ -2,9 +2,50 @@ library(tidyverse)
 library(haven)
 library(rcces)
 
-cc16 <- read_dta("data/source/cces/2016_cc.dta")
+load("data/output/01_responses/common_all.RData")
+
+# birth year
+age_16 <- cc16 %>% 
+  transmute(case_id, 
+            birthyr = as.character(as_factor(birthyr))) %>% 
+  mutate(birthyr = as.Date(str_c(birthyr, "-01-01"), "%Y-%m-%d"))
+stopifnot(all(age_16$birthyr <= 1998))
+write_sav(age_16, "data/output/01_responses/cc16_age.sav")
 
 
+newDataset(age_16, name = "CCES 2016 birthyr")
+
+library(crunch)
+login()
+ds16 <- loadDataset("CCES 2016 Common Vote Validated", project = "CCES")
+# forkDataset(ds16, name = "Fork of CCES 2016")
+
+f16 <- loadDataset("Fork of CCES 2016")
+deleteVariables(f16, "birthyr")
+
+a16 <- loadDataset("CCES 2016 birthyr")
+name(a16$V101) <- "Case Identifier 2016"
+type(a16$V101) <- "text"
+name(a16$birthyr) <- "Year of Birth"
+# GUI Edit for display
+description(a16$birthyr) <- "In what year were you born?"
+
+extendDataset(x = f16, y = a16, by = "V101")
+f16 <- refresh(f16)
+
+# manually edit groupings
+
+# merge
+saveVersion(f16, "merged in birthyr as time variable")
+mergeFork(ds16, fork = f16)
+
+
+
+
+
+
+
+# export for SD
 cc16_hou <- cc16 %>%
   select(inputstate, cdid113,  cdid115,  matches("CurrentHouseName"), matches("HouseCand")) %>% 
   select(-matches("post")) %>% 
