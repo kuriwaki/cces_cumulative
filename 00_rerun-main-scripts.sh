69628d486f1c6144c3e6cb3868f9a7e46a80fdc7
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-03-26 09:11:46 -0400

    re-run with 2018. variables that look off are weight (NAs), age, faminc (NAs), add no vv warning.

--- 00_rerun-main-scripts.sh
+++ 00_rerun-main-scripts.sh
@@ -6,5 +6,5 @@ Rscript 04_stack-cumulative.R
 Rscript 05_extract-contextual.R
 Rscript 06_merge-contextual_upload.R
 # Rscript 07_format-crunch.R
-Rscript -e "rmarkdown::render('guide/guide_cumulative_2006_2017.Rmd')"
+Rscript -e "rmarkdown::render('guide/guide_cumulative_2006_2018.Rmd')"
 
