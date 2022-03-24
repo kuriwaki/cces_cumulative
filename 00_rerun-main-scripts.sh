2caaac171342c68cacca65aada741e0454f6703e
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2022-03-24 10:51:22 -0700

    Full run

--- 00_rerun-main-scripts.sh
+++ 00_rerun-main-scripts.sh
@@ -1,7 +1,7 @@
 # Run scripts
-# Rscript 01_define-names-labels.R || exit 1
-# Rscript 03_read-common.R || exit 1
-# Rscript 04_prepare-fixes.R || exit 1
+Rscript 01_define-names-labels.R || exit 1
+Rscript 03_read-common.R || exit 1
+Rscript 04_prepare-fixes.R || exit 1
 Rscript 05_stack-cumulative.R || exit 1
 Rscript 06_extract-politicians.R || exit 1
 Rscript 07_merge-contextual_upload.R || exit 1
