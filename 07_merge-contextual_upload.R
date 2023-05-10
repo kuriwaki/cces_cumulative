77141088840184edf945fcabce8d4cc2375bafef
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2023-05-10 14:50:46 -0400

    Update to 2022 filename -- rerun tabulations to tex

--- 07_merge-contextual_upload.R
+++ 07_merge-contextual_upload.R
@@ -275,11 +275,11 @@ ccc_factor <-   ccc_fac |>
 # Save ---------
 cli_h1("Save Final data")
 # write sav first for crunch. save RDS and write to dta after applying variable labels in 05
-write_rds(ccc_df, "data/release/cumulative_2006-2021_addon.rds")
+write_rds(ccc_df, "data/release/cumulative_2006-2022_addon.rds")
 
 # anti-join things not to put on dataverse (panel, module)
 panel_charid <- mutate(panel_ids, case_id = as.character(case_id)) # for crunch
-write_rds(anti_join(ccc_df, panel_ids), "data/release/cumulative_2006-2021.rds", compress = "xz")
+write_rds(anti_join(ccc_df, panel_ids), "data/release/cumulative_2006-2022.rds", compress = "xz")
 
 
 # remove panel cases
@@ -290,9 +290,9 @@ for (v in colnames(ccc_common)) {
   attributes(ccc_common[[v]])$label <- ccc_meta$name[which(ccc_meta$alias == v)]
 }
 
-write_rds(ccc_common, "data/output/cumulative_2006-2021_factor.rds")
+write_rds(ccc_common, "data/output/cumulative_2006-2022_factor.rds")
 
-write_dta(ccc_common, "data/release/cumulative_2006-2021.dta", version = 14)
+write_dta(ccc_common, "data/release/cumulative_2006-2022.dta", version = 14)
 
 
 # might write to crunch
